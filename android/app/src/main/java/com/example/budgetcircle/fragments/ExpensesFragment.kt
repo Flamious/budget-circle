82e8182a21f867ca491f74525303deef63f344cf
Flamious <sokol_sokolov@list.ru>
2021-09-15 12:16:16 +0300

    Create test zero value for expenses

--- android/app/src/main/java/com/example/budgetcircle/fragments/ExpensesFragment.kt
+++ android/app/src/main/java/com/example/budgetcircle/fragments/ExpensesFragment.kt
@@ -22,11 +22,19 @@ class ExpensesFragment : Fragment() {
     }
 
     private fun setChart() {
-        val values = arrayListOf(50f, 20f, 15f, 10f, 5f, 92f, 11f, 3f)
+        /*val values = arrayListOf(50f, 20f, 15f, 10f, 5f, 92f, 11f, 3f)*/
+        val values = arrayListOf(0f, 0f, 0f)
+        var i: Float = 0f
+        for(n in values) {
+          i += n
+        }
         val titles = resources.getStringArray(R.array.titles).toCollection(ArrayList())
         val colors = resources.getIntArray(R.array.expense_colors).toCollection(ArrayList())
-
-        PieChartSetter.setChart(titles, values, colors, binding.expensesPieChart)
+        if (i > 0)
+            PieChartSetter.setChart(titles, values, colors, binding.expensesPieChart)
+        else
+            PieChartSetter.setChart(arrayListOf("No entries"), arrayListOf(100f),
+                arrayListOf(resources.getColor(R.color.noExpenses)), binding.expensesPieChart)
     }
 
     companion object {
