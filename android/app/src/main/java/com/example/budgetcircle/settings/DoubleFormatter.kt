98d486f5ec020b24381d9d0d948d8e54c1514680
Sokolov Daniil <sokol_sokolov@list.ru>
2021-11-14 17:01:14 +0300

    Add Russiaan (2)

--- android/app/src/main/java/com/example/budgetcircle/settings/DoubleFormatter.kt
+++ android/app/src/main/java/com/example/budgetcircle/settings/DoubleFormatter.kt
@@ -3,7 +3,7 @@ package com.example.budgetcircle.settings
 class DoubleFormatter {
     companion object {
         fun format(number: Double): Double {
-            return String.format("%.2f", number).toDouble()
+            return String.format("%.2f", number).replace(",", ".").toDouble()
         }
     }
 }
